a87391cb89a29a934ec2aa19f49cfe0bb6f39352
Gustavo Macena <102830712+gustmacena@users.noreply.github.com>
2022-10-14 20:36:39 -0300

    Update tabuada.py

--- tabuada.py
+++ tabuada.py
@@ -1,4 +1,4 @@
-# 3 - Escreva um programa que exiba a tabuada do número digitado, onde o usuário possa digitar
+# O programa que exibe a tabuada do número digitado, onde o usuário possa digitar
 # o inicio e o fim da tabuada.
 n = int(input("Digite um numero da tabuada:"))
 i = int(input("Digite o numero de inicio da tabuada:"))
